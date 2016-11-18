ade0bc966e71dd2774202113373551c9a9fd9553
William Martins <wwwhmartins@gmail.com>
2016-11-18 00:15:08 -0200

    Improve function to generate charts

--- neat/get-fitness-by-population.sh
+++ neat/get-fitness-by-population.sh
@@ -14,7 +14,7 @@ get_fitness_by_population() {
   declare -r pop_folder=${1}
   declare -r pop_files=$( ${UTILS_PATH}/get-population-list.sh ${pop_folder} )
 
-  echo "Generation min(all) avg(all) max(all) min(win) avg(win) max(win)"
+  echo "Generation avg(all) max(all) avg(win)"
   for pop in ${pop_files}; do
     declare pop_number=$(echo ${pop} | sed 's/[^0-9]*//g')
     declare all_fitness=$(
@@ -24,8 +24,8 @@ get_fitness_by_population() {
       ${UTILS_PATH}/get-winners.sh ${pop_folder}/${pop} | xargs \
       ${UTILS_PATH}/get-organism-fitness.sh ${pop_folder}/${pop}
     )
-    declare all_results=$(echo "${all_fitness[*]}" | ${NUM} min avg max)
-    declare winner_results=$(echo "${winners_fitness[*]}" | ${NUM} min avg max)
+    declare all_results=$(echo "${all_fitness[*]}" | ${NUM} avg max)
+    declare winner_results=$(echo "${winners_fitness[*]}" | ${NUM} avg)
     echo "${pop_number} ${all_results} ${winner_results}"
   done
 }
